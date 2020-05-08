baf16b1777375e098fcb05c1824753016a55c914
Crowdbotics <team@crowdbotics.com>
2020-05-08 02:37:09 +0000

    Update models

--- backend/see_me_fly_16746/wsgi.py
+++ backend/see_me_fly_16746/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'see_me_fly_16746.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "see_me_fly_16746.settings")
 
 application = get_wsgi_application()
